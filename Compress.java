9ab7b3f0477b9120a6c8e13915bc8d8614f33ef5
Dalton J Kohl <dkohl@bec307dl01.sandiego.edu>
2021-04-20 15:55:03 -0700

    long way to go

---
+++ Compress.java
@@ -0,0 +1,91 @@
+import java.util.*;
+import java.io.*;
+
+interface KWHashMap<K, V>{
+	V get (K key);
+	boolean isEmpty();
+	V put (K key, V value);
+}
+
+
+public class Compressor{
+	public class HashTableChain<K,V> implements KWHashMap<K,V>{
+		public static class Entry<K,V>{
+			private final K key;
+			private V value;
+
+			public Entry<K k, V v>{
+				key = k;	
+				value = v;
+			}
+
+			public K getKey(){
+				return key;
+			}
+
+			public V getValue(){
+				return value;
+			}
+
+			public V setValue(V v){
+				V old = value;
+				value = v;
+				return old;
+			}
+
+			public String toString(){
+				return key.toString() + " = " + value.toString();
+			}
+
+			private LinkedList<Entry<K,V>[] table;
+			private int numKeys;
+			private static final int CAPACITY = 0; //**NEEDS CHANGED**//
+			private static final double LOAD_THRESHOLD = 0; //**NEEDS CHANGED**//
+
+			public HashTableChain(){
+				table = new LinkedList[CAPACITY];
+				numKeys = 0;
+			}
+
+			public HashTableChain(int capacity){
+				table = new LinkedList[capacity];
+				numKeys = 0;
+			}
+
+			public V get(Object key){
+				int index = key.hashCode() % table.length;
+				if(index < 0){
+					index += table.length;
+				}
+				if(table[index] == null){
+					return null;
+				}
+				for(Entry<K,V> nextItem: table[index]){
+					if(nextItem.getKey().equals(key){
+						return nextItem.getValue();
+					}
+				}
+				return null
+			}
+			
+			public V put(K key, V value){
+				int index = key.hashcode() % table.length;
+				if(index < 0){
+					index += table.length;
+				}
+				if(table[index] == null){
+					table[index] = new LinkedList<Entry<K,V>>;
+				}
+				for(Entry<K,V> nextItem: table[index]){
+					if(nextItem.get(key) == key){
+						return nextItem.setValue(value);
+					}
+					else{
+					
+					}
+					
+
+
+
+
+
