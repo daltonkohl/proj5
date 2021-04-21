def2afa9e0cc51a3d58c6d4ed5dded6d9585164a
Dalton J Kohl <dkohl@bec307dl01.sandiego.edu>
2021-04-21 16:20:49 -0700

    working on driver

--- Compress.java
+++ Compress.java
@@ -10,16 +10,36 @@ public class Compress{
 		BufferedReader input = new BufferedReader(new FileReader(inputFile));
 		String line;
 		StringTokenizer tokens;
+		String outputString;
 		while((line = input.readLine()) != null){
 			line += "\n";
 			tokens = new StringTokenizer(line, "");
 			String inputString = tokens.nextToken();
+			String oldInput = inputString;
 			while(tokens.hasMoreTokens()){
-				if(
+				if(hashTable.get(getKey(inputString)) == null){
+					outputString += getKey(oldInput);
+					hashTable.put(getKey(inputString), inputString);
+					inputString = nextToken();  //MIGHT BE WRONG
+				}
+				else{
+					oldInput = inputString;
+					inputString += tokens.nextToken();
+				}
+
 	
 		/*	HashTableChain<String, Integer> hash = new HashTableChain<>();
 		hash.put("hello", 125);
 		System.out.println(hash.get("hello"));*/
+	}
+	public static String getKey(String value){
+		String outputKey = "";
+		for(int i = 0; i<value.length(); i++){
+			int output =  value.charAt(i);
+			String keyValue = String.valueOf(output);
+			outputKey += keyValue;
+		}
+		return outputKey;
 	}
 }
 
